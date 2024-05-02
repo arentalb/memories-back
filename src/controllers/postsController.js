0699e0877c835a9aeff9ce8cb59d495628fc8c9d
aren talb <aren.talb00@gmail.com>
2024-05-02 17:17:43 +0300

    Implement updating post and getting post by id

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,10 +1,27 @@
 import PostMessage from "../model/postMessageModel.js";
+import mongoose from "mongoose";
 
 export async function getPosts(req, res) {
   try {
     const postMessages = await PostMessage.find();
-    console.log(postMessages);
     res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ msg: error.message });
+  }
+}
+
+export async function getPostById(req, res) {
+  const postId = req.params.id;
+  if (postId === null)
+    return res
+      .status(404)
+      .json({ msg: "This post dose not exist with that id " });
+
+  try {
+    const postMessages = await PostMessage.findById(postId);
+    if (postMessages) res.status(200).json(postMessages);
+    else
+      res.status(404).json({ msg: "This post dose not exist with that id " });
   } catch (error) {
     res.status(404).json({ msg: error.message });
   }
@@ -17,6 +34,21 @@ export async function createPost(req, res) {
   try {
     await newPost.save();
     res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ msg: error.message });
+  }
+}
+
+export async function updatePost(req, res) {
+  const post = req.body;
+  const { id: _id } = req.params;
+
+  try {
+    if (!mongoose.Types.ObjectId.isValid(_id)) {
+      return res.status(404).send("No post exists with that id ");
+    }
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post);
+    res.status(201).json(updatedPost);
   } catch (error) {
     res.status(409).json({ msg: error.message });
   }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { createPost, getPosts } from "../controllers/postsController.js";
+import {
+  createPost,
+  getPostById,
+  getPosts,
+  updatePost,
+} from "../controllers/postsController.js";
 
 const router = express.Router();
 
 router.get("/", getPosts);
 router.post("/", createPost);
+router.patch("/:id", updatePost);
+router.get("/:id", getPostById);
 
 export default router;
